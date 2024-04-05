b03f66315a4346e47c4c3b9cfbe59e581fb02baf
Wattchman <tamiriraishetekede@gmail.com>
2024-04-05 11:31:53 +0100

    new changes

--- Flane/settings.py
+++ Flane/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,7 +93,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+DATABASES = {
+    "default": dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
