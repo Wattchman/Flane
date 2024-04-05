1160dc916c48e38c5ae8d342c674603e2eac4dbb
Wattchman <143461210+Wattchman@users.noreply.github.com>
2024-04-05 13:39:00 +0200

    Update settings.py

--- Flane/settings.py
+++ Flane/settings.py
@@ -92,7 +92,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
