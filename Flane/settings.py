33abbb895ed0e60ca5a31201b1e91860e5e5bb44
Wattchman <143461210+Wattchman@users.noreply.github.com>
2024-04-06 01:03:44 +0100

    Update settings.py

--- Flane/settings.py
+++ Flane/settings.py
@@ -143,6 +143,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 REDIS_HOST = dj_redis_url.parse(os.environ.get("REDIS_URL"))
 REDIS_PORT = 6379
+REDIS_DB = 0
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
