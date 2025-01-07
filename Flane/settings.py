23f587bbdd2ae6de3de7b219c9e279134c6a6273
Wattchman <143461210+Wattchman@users.noreply.github.com>
2025-01-07 23:29:13 +0200

    Update settings.py

--- Flane/settings.py
+++ Flane/settings.py
@@ -139,9 +139,8 @@ STATICFILES = (os.path.join(BASE_DIR, 'static'),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/mnt/data/media'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/mnt/data/media'
 #REDIS_HOST = dj_redis_url.parse(os.environ.get("REDIS_URL"))
 #REDIS_PORT = 6379
 #REDIS_DB = 0
