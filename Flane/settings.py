0fc5193831d0c84989a556cc70cfaaf976239a5e
Wattchman <143461210+Wattchman@users.noreply.github.com>
2025-01-14 01:59:01 +0200

    Update settings.py

--- Flane/settings.py
+++ Flane/settings.py
@@ -162,5 +162,15 @@ MEDIA_ROOT = '/mnt/data/media'
     # }
 # }
 
+# Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+# Email Settings
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'Tekede96@gmail.com'
+EMAIL_HOST_PASSWORD = 'Munashe6919'
+
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
