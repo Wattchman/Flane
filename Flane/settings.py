b598432d6e84f37d3971416a93944e6f897fc98f
Wattchman <143461210+Wattchman@users.noreply.github.com>
2025-01-14 09:58:17 +0200

    Update settings.py

--- Flane/settings.py
+++ Flane/settings.py
@@ -166,7 +166,7 @@ MEDIA_ROOT = '/mnt/data/media'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Email Settings
-EMAIL_HOST = 'smtp.gmail.com
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'Tekede96@gmail.com'
