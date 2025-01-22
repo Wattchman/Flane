e18910af341d5b214d934e3ec11625990e2cf9bb
Wattchman <143461210+Wattchman@users.noreply.github.com>
2025-01-22 23:43:40 +0200

    Update views.py

--- maricho/views.py
+++ maricho/views.py
@@ -588,7 +588,8 @@ def job_list(request):
         )
 
     # Filter jobs by industry, if selected
-
+     if industry_id:
+        jobs = jobs.filter(industry_id=industry_id)
 
     # Pass the data to the template
     context = {
