d2601571193a57dfcdca78049b9b57739b5658ea
Wattchman <143461210+Wattchman@users.noreply.github.com>
2025-02-14 00:42:48 +0200

    Update custom_filters.py

--- maricho/templatetags/custom_filters.py
+++ maricho/templatetags/custom_filters.py
@@ -1,4 +1,17 @@
+import re
 from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name="bold_subheadings")
+def bold_subheadings(value):
+    """
+    Automatically makes subheadings bold. Detects lines that start with ## and wraps them in <strong> tags.
+    """
+    pattern = r"(?m)^(##\s*)(.*)"
+    formatted_text = re.sub(pattern, r"<strong>\2</strong>", value)
+    return mark_safe(formatted_text)
 register = template.Library()
 
 @register.filter
